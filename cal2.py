d8247a65ada9b40b2df6927653976d00b5c6373a
eunseon <kwon9348@naver.com>
2021-11-24 17:04:10 +0900

    print 2

--- cal2.py
+++ cal2.py
@@ -2,3 +2,9 @@ class MoreFourCal(FourCal):
     def pow(self):
         result = self.first ** self.second
         return result
+    def div(self):
+        if self.second == 0:
+            return 0
+        else:
+            return self.first/self.second
+
